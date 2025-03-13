26106f5387a8f15845dd3d43c6abb4cfde107b66
tymyevhenmpw <tymyevhen@gmail.com>
2025-03-13 09:38:42 +0100

    Initial commit

---
+++ constants/store.js
@@ -0,0 +1,12 @@
+export const Store = {
+  "name": "FaD.PRO",
+  "currency": "UAH",
+  "currency_sign": "₴",
+  "domain": "https://fo-scandinavia.vercel.app",
+  "database": "FaDPRO_2025_03_13",
+  "default_image": {
+    "product_card": 0,
+    "product_page": 0,
+    "checkout_page": 0
+  }
+};
